fad51a972d1eab03a2e095849d10267e15b30842
root <root@7735277998e8>
2017-06-27 00:51:43 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_EQbPiMLmvo",
+  "homePage" : "Main",
+  "name" : "Auto_EQbPiMLmvo",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
